4cc438094ae16cca83f8c5035a63aac287ee9f2f
Donald Wade <rwckdw@gmail.com>
2017-10-21 20:04:16 +1100

    Fix webpack config. Remove incorrect syntax.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-import path from 'path';
+const path = require('path');
 
 const config = {
   entry: './src/index.js',
@@ -8,4 +8,4 @@ const config = {
   }
 };
 
-export default config;
+module.exports = config;
